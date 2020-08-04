8d2b50de704287e4af80bfe46897610b5f6d97c9
DenisLay <56198059+DenisLay@users.noreply.github.com>
2020-08-04 14:51:26 +0300

    no collision

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+
+    private Rigidbody2D rigidbody;
+    private bool inAir;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(direction, 0).normalized * moveSpeed);
+        
+        if (Input.GetKey(KeyCode.Space) && !inAir)
+        {
+            inAir = true;
+            rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    private void OnColissionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.tag == "Ground")
+        {
+            inAir = false;
+        }
+
+        Debug.Log(collision.gameObject.name);
+    }
+}
